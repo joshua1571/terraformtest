505385156d4ae9bb8ac642b5e665e6ef1a649011
Joshua Hernandez <joshua1571@users.noreply.github.com>
2021-11-24 11:23:04 -0800

    ubuntuvm test

--- variables.tf
+++ variables.tf
@@ -2,3 +2,10 @@ variable "resource_group_name" {
   default = "myTFResourceGroup"
 }
 
+variable "prefix" {
+  default = "tfvm"
+}
+
+variable "public_ssh_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCzvZkjzezv0hJ3ZgIj5rVurQG3kHDIG0jPcy6mYYHyiS7tOmUutF4c+bDqBQL3mXr+7I31KhQFPvDSrMAx7jAqVDssDG8AimR0sDC0/9J8qqMSmBH+SQXuVdykPAY7B2ix0eGi4VpDcHjAlT5+e9SvDtl4Ifzp3WJAfDka+h748wDZicd4YEf5WzuI4wV6lZ3o+VIVDUH+Esw5PQGMTUlCh0zFSUJUjzd9/ADteD4vrz/s3gId4VfVMGiGoyx5M5iKI1g8WDOk7okU2g71nOkBJ8GlajouQ3kJvHe4+zuVP+y2oXY6YkPDzahRtpcMt0v/ZJF+OrCRbFktT6eWqUWbl0MTtHt0nLzQhz1IP4xcRJVhk8qurkw8jY91mVxseNlahonde6yYfRVRx4Ivy12S+1akuUPR11rRyUgjTJISLubADz7+lRn5RuL1bWKTTsQmFeyHxjIAZ8sOqVs/+2j9jSjkIic5wXXw2BcQNj0iF8gnAtqBHVV/f8X5167vm8M= jrh@JRH-MacBook-Pro"
+}
